c6b028e301e07f861b25b4f1a930f395c9d74676
kapil-varshney <kvarshney.14@gmail.com>
2018-02-14 20:03:04 +0530

    submitting Assignemnt 1

---
+++ kapil/ex_0100.py
@@ -0,0 +1,13 @@
+"""
+Assignment 1 : Calculate pay
+"""
+
+#Ask user to enter the hours and rate
+hrs = float(input("Enter hours: "))
+rate = float(input("Enter rate per hour: "))
+
+#Calculate pay
+pay = hrs*rate
+
+#print the calculated pay
+print ("The pay is", pay)
