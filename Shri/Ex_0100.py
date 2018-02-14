a2cbcf16a343f4f6da4c8e883a4c410e9f470ba6
ShriRamani <ramanisr.msu@gmail.com>
2018-02-14 20:02:43 +0530

    shri

--- Shri/Ex_0100.py
+++ Shri/Ex_0100.py
@@ -1,1 +1,11 @@
-print ("hello world")
+#To calculate pay
+
+hours = input ("How many hours of work")
+Pay = input ("rate of pay per hour")
+
+Workinghours = float(hours)
+Payperhour = float(Pay)
+
+x = Workinghours * Payperhour
+
+print(x)
